0eada6f1d202739389afd6f0ba6c5de68586adc3
Atul Singh <30928157+AtulSinghRathore@users.noreply.github.com>
2024-02-07 01:58:03 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,183 @@
+'use strict';
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * PRELOADER
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("DOMContentLoaded", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * NAVBAR
+ * navbar toggle for mobile
+ */
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggleBtn.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+// JavaScript for modal functionality
+document.addEventListener('DOMContentLoaded', function() {
+  const layerLinks = document.querySelectorAll('.layer-link');
+
+  layerLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent the default behavior of the anchor tag
+      const modalTitle = this.parentElement.querySelector('.card-title').innerText;
+      const modalDescription = this.parentElement.querySelector('.card-text').innerText;
+
+      document.getElementById('modal-title').innerText = modalTitle;
+      document.getElementById('modal-description').innerText = modalDescription;
+
+      document.getElementById('myModal').style.display = 'block';
+    });
+  });
+
+  // Close the modal when the close button is clicked
+  const closeBtn = document.querySelector('.close');
+  closeBtn.addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'none';
+  });
+
+  // Close the modal when clicking outside of it
+  window.addEventListener('click', function(event) {
+    const modal = document.getElementById('myModal');
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
+
+  
+
+
+
+/**
+ * HEADER
+ * header active when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * SLIDER
+ */
+
+const sliders = document.querySelectorAll("[data-slider]");
+
+const initSlider = function (currentSlider) {
+
+  const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+  let totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
+  let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+  let currentSlidePos = 0;
+
+  const moveSliderItem = function () {
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+  }
+
+  /**
+   * NEXT SLIDE
+   */
+  const slideNext = function () {
+    const slideEnd = currentSlidePos >= totalSlidableItems;
+
+    if (slideEnd) {
+      currentSlidePos = 0;
+    } else {
+      currentSlidePos++;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderNextBtn.addEventListener("click", slideNext);
+
+  /**
+   * PREVIOUS SLIDE
+   */
+  const slidePrev = function () {
+    if (currentSlidePos <= 0) {
+      currentSlidePos = totalSlidableItems;
+    } else {
+      currentSlidePos--;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderPrevBtn.addEventListener("click", slidePrev);
+
+  const dontHaveExtraItem = totalSlidableItems <= 0;
+  if (dontHaveExtraItem) {
+    sliderNextBtn.style.display = 'none';
+    sliderPrevBtn.style.display = 'none';
+  }
+
+  /**
+   * slide with [shift + mouse wheel]
+   */
+
+  currentSlider.addEventListener("wheel", function (event) {
+    if (event.shiftKey && event.deltaY > 0) slideNext();
+    if (event.shiftKey && event.deltaY < 0) slidePrev();
+  });
+
+  /**
+   * RESPONSIVE
+   */
+
+  window.addEventListener("resize", function () {
+    totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
+    totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+    moveSliderItem();
+  });
+
+}
+
+for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
